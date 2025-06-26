e7fb686b5c764f24bf305d148fdb5023ad99a060
Luka <luigi7up@gmail.com>
2025-06-26 12:54:19 +0200

    Switched to one.com hotjar code

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ import mp from '../mixpanel';
 // Hotjar Tracking Code
 (function(h,o,t,j,a,r){
     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:6446586,hjsv:6};
+    h._hjSettings={hjid:6446721,hjsv:6};
     a=o.getElementsByTagName('head')[0];
     r=o.createElement('script');r.async=1;
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
